30a4ec6ed58f9872eb005ed2306454aa908df447
EslamKamel89 <elsamkamelforex@gmail.com>
2026-07-02 23:00:28 +0300

    feat(core): add Book model with user many-to-many relationship

--- core/models.py
+++ core/models.py
@@ -1,6 +1,24 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    books = models.ManyToManyField("Book", related_name="users")
+
+
+class Book(models.Model):
+    class GenreChoices(models.TextChoices):
+        FICTION = "Fiction"
+        NON_FICTION = "Non-Fiction"
+        MYSTERY = "Mystery"
+
+    name = models.CharField(max_length=255)
+    genre = models.CharField(max_length=24, choices=GenreChoices.choices)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ("-created_at",)
+
+    def __str__(self) -> str:
+        return self.name
